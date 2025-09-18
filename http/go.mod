686408363637f11e4c1833cb870898d9d3f5f2f7
Dima Osin <sevmor2016@yandex.ru>
2025-09-18 13:41:41 +0300

    repo layer - link get methods

--- http/go.mod
+++ http/go.mod
@@ -3,16 +3,18 @@ module github.com/dfg007star/avito_informer/http
 go 1.25.1
 
 require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/jackc/pgx/v5 v5.7.6
 	gorm.io/gorm v1.31.0
 )
 
 require (
-	github.com/dfg007star/avito_informer v0.0.0-20250918091036-102cc9d871f4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 )
--- http/internal/repository/link/get.go
+++ http/internal/repository/link/get.go
@@ -25,8 +25,8 @@ func (r *repository) GetAllLinks(ctx context.Context) ([]*model.Link, error) {
 	var links []*model.Link
 	for rows.Next() {
 		var l model.Link
-		// Replace these fields with actual fields from your model.Link
 		err := rows.Scan(
+			&l.ID,
 			&l.Name,
 			&l.Url,
 			&l.CreatedAt,
@@ -43,4 +43,88 @@ func (r *repository) GetAllLinks(ctx context.Context) ([]*model.Link, error) {
 	}
 
 	return links, nil
+}
+
+func (r *repository) GetLinkById(ctx context.Context, id string) (*model.Link, error) {
+	query, args, err := squirrel.Select("*").
+		From("links").
+		Where(squirrel.Eq{"id": id}).
+		PlaceholderFormat(squirrel.Dollar).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %w", err)
+	}
+
+	row := r.db.QueryRow(ctx, query, args...)
+
+	var link model.Link
+
+	err = row.Scan(
+		&link.ID,
+		&link.Name,
+		&link.Url,
+		&link.CreatedAt,
+		&link.ParsedAt,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan row: %w", err)
+	}
+
+	return &link, nil
+}
+
+func (r *repository) GetLinkItems(ctx context.Context, link *model.Link) ([]*model.Item, error) {
+	queryBuilder := squirrel.Select(
+		"id",
+		"link_id",
+		"uid",
+		"title",
+		"description",
+		"url",
+		"preview_url",
+		"price",
+		"need_notify",
+		"created_at",
+	).
+		From("items").
+		Where(squirrel.Eq{"link_id": link.ID}).
+		PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := queryBuilder.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %w", err)
+	}
+
+	rows, err := r.db.Query(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %w", err)
+	}
+	defer rows.Close()
+
+	var items []*model.Item
+	for rows.Next() {
+		var item model.Item
+		err := rows.Scan(
+			&item.ID,
+			&item.LinkID,
+			&item.Uid,
+			&item.Title,
+			&item.Description,
+			&item.Url,
+			&item.PreviewUrl,
+			&item.Price,
+			&item.NeedNotify,
+			&item.CreatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan item: %w", err)
+		}
+		items = append(items, &item)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows iteration error: %w", err)
+	}
+
+	return items, nil
 }
