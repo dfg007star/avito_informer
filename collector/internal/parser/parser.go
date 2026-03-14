9d2286aae5ecbaa701d2292413488ab9628916a4
Dima Osin <sevmor2016@yandex.ru>
2026-03-14 14:47:49 +0300

    adding proxy to chromedp

--- collector/internal/parser/parser.go
+++ collector/internal/parser/parser.go
@@ -2,9 +2,9 @@ package parser
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"math/rand"
+	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
@@ -14,7 +14,6 @@ import (
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/fetch"
 	"github.com/chromedp/cdproto/network"
-	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 	"github.com/dfg007star/avito_informer/collector/internal/config"
 	"github.com/dfg007star/avito_informer/collector/internal/model"
@@ -22,186 +21,126 @@ import (
 
 var userAgents = []string{
 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 YaBrowser/132.0.0.0 Yowser/2.5 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Vivaldi/4.3.2439.65 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 OPR/80.0.4170.72 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Brave/1.32.106 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Iron/132.0.0.0 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Sleipnir/6.4.10 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Comodo_Dragon/132.0.0.0 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Maxthon/6.1.0.2000 Safari/537.36",
 }
 
 type Parser struct {
 	AllocatorCtx    context.Context
 	CancelAllocator context.CancelFunc
+	ProxyUser       string
+	ProxyPass       string
 }
 
 func NewParser() *Parser {
+	proxy := config.AppConfig().Parser.Proxy()
 	opts := []chromedp.ExecAllocatorOption{
 		chromedp.Flag("headless", true),
-		chromedp.Flag("disable-blink-features", "AutomationControlled"),
 		chromedp.Flag("no-sandbox", true),
-		chromedp.Flag("start-maximized", true),
 		chromedp.Flag("disable-gpu", true),
 		chromedp.Flag("disable-dev-shm-usage", true),
-		chromedp.Flag("disable-extensions", true),
-		chromedp.Flag("disable-infobars", true),
-		chromedp.Flag("disable-setuid-sandbox", true),
 		chromedp.WindowSize(1366, 768),
 		chromedp.UserAgent(userAgents[rand.Intn(len(userAgents))]),
 	}
+
+	var proxyUser, proxyPass string
+	if proxy != "" {
+		if u, err := url.Parse(proxy); err == nil && u.User != nil {
+			proxyUser = u.User.Username()
+			proxyPass, _ = u.User.Password()
+			u.User = nil
+			proxy = u.String()
+		}
+		opts = append(opts, chromedp.ProxyServer(proxy))
+		log.Printf("Parser initialized with proxy: %s", proxy)
+	}
+
 	allocatorCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 
 	return &Parser{
 		AllocatorCtx:    allocatorCtx,
 		CancelAllocator: cancel,
+		ProxyUser:       proxyUser,
+		ProxyPass:       proxyPass,
 	}
 }
 
-func (p *Parser) Parse(link *model.Link, cookies map[string]string) ([]*model.Item, error) {
-	var items []*model.Item
-	priceRegex := regexp.MustCompile(`[^0-9] `)
+// https://github.com/chromedp/examples/blob/master/proxy/main.go
+func (p *Parser) handleProxyAuth(ctx context.Context) {
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		switch ev := ev.(type) {
+		case *fetch.EventRequestPaused:
+			go func() {
+				_ = chromedp.Run(ctx, fetch.ContinueRequest(ev.RequestID))
+			}()
+		case *fetch.EventAuthRequired:
+			if p.ProxyUser != "" && ev.AuthChallenge.Source == fetch.AuthChallengeSourceProxy {
+				log.Printf("Proxy auth required for %s. Providing credentials for user: %s", ev.AuthChallenge.Origin, p.ProxyUser)
+				go func() {
+					err := chromedp.Run(ctx, fetch.ContinueWithAuth(ev.RequestID, &fetch.AuthChallengeResponse{
+						Response: fetch.AuthChallengeResponseResponseProvideCredentials,
+						Username: p.ProxyUser,
+						Password: p.ProxyPass,
+					}))
+					if err != nil {
+						log.Printf("Failed to provide proxy auth: %v", err)
+					}
+				}()
+			} else {
+				log.Printf("Auth required (not proxy): %s", ev.AuthChallenge.Source)
+				go func() {
+					_ = chromedp.Run(ctx, fetch.ContinueWithAuth(ev.RequestID, &fetch.AuthChallengeResponse{
+						Response: fetch.AuthChallengeResponseResponseDefault,
+					}))
+				}()
+			}
+		}
+	})
+}
 
-	taskCtx, cancelTask := chromedp.NewContext(
-		p.AllocatorCtx,
-		chromedp.WithLogf(log.Printf),
-	)
+func (p *Parser) Parse(link *model.Link, cookies map[string]string) ([]*model.Item, error) {
+	taskCtx, cancelTask := chromedp.NewContext(p.AllocatorCtx)
 	defer cancelTask()
 
-	taskCtx, cancelTimeout := context.WithTimeout(taskCtx, 45*time.Second)
+	taskCtx, cancelTimeout := context.WithTimeout(taskCtx, 60*time.Second)
 	defer cancelTimeout()
 
-	var err error
-	maxRetries := config.AppConfig().Parser.GetItemsMaxRetry()
-	for i := 0; i < maxRetries; i++ {
-		var res string
-		err = chromedp.Run(taskCtx,
-			network.Enable(),
-			fetch.Enable(),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				chromedp.ListenTarget(ctx, func(ev interface{}) {
-					if reqEvent, ok := ev.(*fetch.EventRequestPaused); ok {
-						go func() {
-							if reqEvent.ResourceType == network.ResourceTypeImage {
-								err := fetch.FailRequest(reqEvent.RequestID, network.ErrorReasonBlockedByClient).Do(ctx)
-								if err != nil {
-									log.Printf("failed to block request %s: %v", reqEvent.Request.URL, err)
-								}
-							} else {
-								err := fetch.ContinueRequest(reqEvent.RequestID).Do(ctx)
-								if err != nil {
-									log.Printf("failed to continue request %s: %v", reqEvent.Request.URL, err)
-								}
-							}
-						}()
-					}
-				})
+	p.handleProxyAuth(taskCtx)
 
-				script := `
-					Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
-					Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
-					Object.defineProperty(navigator, 'vendor', { get: () => 'Google Inc.' });
-					window.chrome = { runtime: {} };
-					Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] });
-					Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-				`
-				_, err := page.AddScriptToEvaluateOnNewDocument(script).Do(ctx)
-				if err != nil {
-					return err
-				}
-
-				for name, value := range cookies {
-					expr := cdp.TimeSinceEpoch(time.Now().Add(7 * 24 * time.Hour))
-					err := network.SetCookie(name, value).
-						WithExpires(&expr).
-						WithDomain("www.avito.ru").
-						WithHTTPOnly(false).
-						Do(ctx)
-					if err != nil {
-						return err
-					}
-				}
-				return nil
-			}),
-			chromedp.Navigate(link.Url),
-			chromedp.WaitReady("body"),
-			chromedp.WaitVisible("div[data-marker='item']", chromedp.ByQuery),
-			chromedp.OuterHTML("html", &res),
-		)
-
-		if err == nil {
-			doc, err := goquery.NewDocumentFromReader(strings.NewReader(res))
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	var res string
+	err := chromedp.Run(taskCtx,
+		fetch.Enable().WithHandleAuthRequests(true),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			for name, value := range cookies {
+				expr := cdp.TimeSinceEpoch(time.Now().Add(7 * 24 * time.Hour))
+				_ = network.SetCookie(name, value).WithExpires(&expr).WithDomain("www.avito.ru").Do(ctx)
 			}
-
-			doc.Find("div[data-marker='item']").Each(func(i int, s *goquery.Selection) {
-				uid := s.AttrOr("data-item-id", "")
-				priceStr := s.Find("meta[itemprop='price']").AttrOr("content", "")
-				priceStr = priceRegex.ReplaceAllString(priceStr, "")
-				price, _ := strconv.Atoi(priceStr)
-				//var itemPreviewUrl string
-				//if imgSelection := s.Find("img[itemprop='image']"); imgSelection.Length() > 0 {
-				//	itemPreviewUrl = imgSelection.AttrOr("src", "")
-				//}
-
-				var imageUrls []string
-				dataMarkerRegex := regexp.MustCompile(`slider-image/image-(https?://.*)`)
-				s.Find("ul.photo-slider-list-R0jle li").Each(func(_ int, li *goquery.Selection) {
-					dataMarker, exists := li.Attr("data-marker")
-					if exists {
-						matches := dataMarkerRegex.FindStringSubmatch(dataMarker)
-						if len(matches) > 1 {
-							imageUrls = append(imageUrls, matches[1])
-						}
-					}
-				})
-
-				item := &model.Item{
-					LinkId:      link.ID,
-					Uid:         uid,
-					Title:       s.Find("h2[itemprop='name']").Text(),
-					Price:       price,
-					Description: s.Find("meta[itemprop='description']").AttrOr("content", ""),
-					Url:         s.Find("a[itemprop='url']").AttrOr("href", ""),
-					PreviewUrl:  imageUrls[0],
-					IsNotify:    link.ItemsCount == 0,
-				}
-				items = append(items, item)
-			})
-			break
-		}
-
-		log.Printf("failed to visit %s (attempt %d/%d): %s", link.Name, i+1, maxRetries, err)
-		if i < maxRetries-1 {
-			time.Sleep(config.AppConfig().Parser.GetItemsRetryDelay())
-		}
-	}
+			return nil
+		}),
+		chromedp.Navigate(link.Url),
+		chromedp.WaitVisible("div[data-marker='item']", chromedp.ByQuery),
+		chromedp.OuterHTML("html", &res),
+	)
 
 	if err != nil {
-		return nil, fmt.Errorf("failed to visit url after %d attempts: %w", maxRetries, err)
+		return nil, err
 	}
 
-	return items, nil
+	return p.parseHTML(res, link)
 }
 
 func (p *Parser) GetCookies(urlStr string) (map[string]string, error) {
 	taskCtx, cancelTask := chromedp.NewContext(p.AllocatorCtx)
 	defer cancelTask()
 
-	taskCtx, cancelTimeout := context.WithTimeout(taskCtx, config.AppConfig().Parser.GetCookieTimeout())
+	taskCtx, cancelTimeout := context.WithTimeout(taskCtx, 60*time.Second)
 	defer cancelTimeout()
 
-	log.Printf("attempting to get cookies from %s", urlStr)
+	p.handleProxyAuth(taskCtx)
 
 	var cookies []*network.Cookie
 	err := chromedp.Run(taskCtx,
-		network.Enable(),
+		fetch.Enable().WithHandleAuthRequests(true),
 		chromedp.Navigate(urlStr),
-		chromedp.WaitReady("body"),
-		chromedp.Sleep(3*time.Second),
+		chromedp.Sleep(5*time.Second),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			var err error
 			cookies, err = network.GetCookies().Do(ctx)
@@ -209,15 +148,41 @@ func (p *Parser) GetCookies(urlStr string) (map[string]string, error) {
 		}),
 	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get cookies: %w", err)
+		return nil, err
 	}
 
 	cookieMap := make(map[string]string)
 	for _, cookie := range cookies {
 		cookieMap[cookie.Name] = cookie.Value
 	}
-
 	return cookieMap, nil
+}
+
+func (p *Parser) parseHTML(res string, link *model.Link) ([]*model.Item, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(res))
+	if err != nil {
+		return nil, err
+	}
+
+	var items []*model.Item
+	priceRegex := regexp.MustCompile(`[^0-9]`)
+	doc.Find("div[data-marker='item']").Each(func(i int, s *goquery.Selection) {
+		uid := s.AttrOr("data-item-id", "")
+		priceStr := priceRegex.ReplaceAllString(s.Find("meta[itemprop='price']").AttrOr("content", ""), "")
+		price, _ := strconv.Atoi(priceStr)
+
+		item := &model.Item{
+			LinkId:      link.ID,
+			Uid:         uid,
+			Title:       s.Find("h2[itemprop='name']").Text(),
+			Price:       price,
+			Description: s.Find("meta[itemprop='description']").AttrOr("content", ""),
+			Url:         s.Find("a[itemprop='url']").AttrOr("href", ""),
+			IsNotify:    link.ItemsCount == 0,
+		}
+		items = append(items, item)
+	})
+	return items, nil
 }
 
 func (p *Parser) Shutdown() {
